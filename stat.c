67d4dcb9567a3e4d986b1d79e6e7b795b1f070cf
IVAN <ivan@>
2022-01-14 12:35:41 +0200

    comment

--- stat.c
+++ stat.c
@@ -8,6 +8,10 @@ int main()
 {
 
 	struct stat buff;
+
+	/* A notorious example where even C++ needs the struct keyword is the POSIX stat system call that uses a struct
+	 * of the same name in its arguments: 
+	 */
 	int x = stat("stat.c", &buff);
 	return 0;
 }
